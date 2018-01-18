117b9cdfb3849ba8ef20946e93b22efa8fec0ad7
Sirajul Mostafa <sirajulmost@gmail.com>
2018-01-18 20:31:48 +0600

    Install .NET Framework For Simple EatmMvc app

---
+++ EAtmMvcSirajulApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EAtmMvcSirajulApp.MvcApplication" Language="C#" %>
---
+++ EAtmMvcSirajulApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EAtmMvcSirajulApp.Startup))]
+namespace EAtmMvcSirajulApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
